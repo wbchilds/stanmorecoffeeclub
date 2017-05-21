405d531a9fbef0e8e5053c568ba1d742ce58d0a8
Andrew <andrew.szwec@gmail.com>
2017-05-22 09:12:39 +1000

    Initial experientation with Floyd and AWS data loading

---
+++ download_data.sh
@@ -0,0 +1,23 @@
+# Fix drive http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ebs-expand-volume.html#recognize-expanded-volume-linux
+
+wget --load-cookies cookies.txt https://www.kaggle.com/c/intel-mobileodt-cervical-cancer-screening/download/train.7z
+7za x train.7z
+
+wget --load-cookies cookies.txt https://www.kaggle.com/c/intel-mobileodt-cervical-cancer-screening/download/test.7z
+7za x test.7z
+
+wget --load-cookies cookies.txt https://www.kaggle.com/c/intel-mobileodt-cervical-cancer-screening/download/removed_files.csv.zip
+7za x removed_files.csv.zip
+
+wget --load-cookies cookies.txt https://www.kaggle.com/c/intel-mobileodt-cervical-cancer-screening/download/fixed_labels_v2.csv.zip
+7za x fixed_labels_v2.csv.zip 
+
+wget --load-cookies cookies.txt https://www.kaggle.com/c/intel-mobileodt-cervical-cancer-screening/download/additional_Type_3_v2.7z
+7za x additional_Type_3_v2.7z
+
+wget --load-cookies cookies.txt https://www.kaggle.com/c/intel-mobileodt-cervical-cancer-screening/download/additional_Type_2_v2.7z
+7za x additional_Type_2_v2.7z 
+
+wget --load-cookies cookies.txt https://www.kaggle.com/c/intel-mobileodt-cervical-cancer-screening/download/additional_Type_1_v2.7z
+7za x additional_Type_1_v2.7z 
+
---
+++ floyd_download_data.sh
@@ -0,0 +1,8 @@
+cd /output
+
+wget --load-cookies cookies.txt https://www.kaggle.com/c/intel-mobileodt-cervical-cancer-screening/download/sample_submission.csv.zip
+unzip sample_submission.csv.zip
+
+
+
+
